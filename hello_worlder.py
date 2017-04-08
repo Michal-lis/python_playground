e2b9ac1ca64a3a2ba5c6c7e291aad9d99e9d4b00
Michał Lis <Micha Lis>
2017-04-08 21:52:00 +0200

    Pierwszy

---
+++ hello_worlder.py
@@ -0,0 +1,19 @@
+bestStudent = {}  # dictionary
+f = open('stud.txt')
+for line in f:
+    name, grade = line.split()
+    bestStudent[grade] = name
+f.close()
+
+bestStudentStr = ""
+
+for i in sorted(bestStudent.keys(), reverse=True):
+    print(bestStudent[i] + " scored a " + i)
+    bestStudentStr += bestStudent[i] + ' scored a ' + i + ".\n"
+print(bestStudentStr)
+
+
+outToFile=open('tworzonyplik.txt', mode = 'w', encoding='utf-8')
+outToFile.write(bestStudentStr)
+
+outToFile.close()
