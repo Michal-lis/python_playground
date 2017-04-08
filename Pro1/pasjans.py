2e1dcf525b09c83ebf8d66867d7fc306c3b9a175
PiotrJander <pjander2@gmail.com>
2017-04-08 22:53:57 +0200

    Adds ranks

--- Pro1/pasjans.py
+++ Pro1/pasjans.py
@@ -1,1 +1,24 @@
-print ("Robimy z Piotrem pasjansa")
+from enum import Enum
+
+
+class Card:
+    def __init__(self, rank, color):
+        super().__init__()
+        self.color = color
+        self.rank = rank
+
+
+class Rank(Enum):
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 15
