b855cca07bffd405d8acaa21019bf33faf88876a
PiotrJander <pjander2@gmail.com>
2017-04-22 20:55:35 +0200

    Adds persons

--- webapp/server.py
+++ webapp/server.py
@@ -1,40 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, g
 import json
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-@app.route("/about")
-def about():
-    return "Foo!"
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # do_the_login()
-        return json.dumps(request.form)
-    else:
-        return "You made a GET request"
-        # show_the_login_form()
+@app.route("/person/<int:id>")
+def person(id):
+    return json.dumps(g.persons[id])
 
 
 if __name__ == "__main__":
-    app.run()
+    with app.app_context():
+
+        g.persons = [
+            {
+                'id': 0,
+                'name': 'Janek',
+                'age': 21
+            },
+            {
+                'id': 1,
+                'name': 'Klaudia',
+                'age': 22
+            }
+        ]
+
+        app.run()
+
+
+# {
+#     'id': 1,
+#     'name': 'Michał',
+#     'age': 21
+# }
