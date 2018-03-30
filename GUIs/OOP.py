8c288ee050d60f28fd65a44fde5ea47dade1c6e3
michal <michallis95@vp.pl>
2018-03-30 14:42:57 +0200

    Tkinter examples

---
+++ GUIs/OOP.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+# *args - any number of arguments
+# **kwargs - keyword arguments, small dicts
+class SeaofBTCapp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+        # minimum size, priority
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, stickyu='nsew')
